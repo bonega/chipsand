da0287376966a55420371a9974a4be0f26f51d7f
Andreas Liljeqvist <bonega@gmail.com>
2020-07-23 16:02:39 +0200

    Support arguments to binary

--- src/bin/chipsand.rs
+++ src/bin/chipsand.rs
@@ -10,12 +10,13 @@ use chipsandlib::{cpu, save_screen_buffer};
 use chipsandlib::display::Display;
 use chipsandlib::input::{from_sdl2_event, Control};
 use chipsandlib::mmu::MMU;
+use anyhow::{Context, Result};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "chipsand")]
 struct Opt {
-    #[structopt(name = "FILE", parse(from_os_str))]
-    file: PathBuf,
+    #[structopt(name = "ROM", parse(from_os_str))]
+    rom: PathBuf,
 }
 
 fn emulation_loop(mut cpu: cpu::CPU) {
@@ -27,18 +28,10 @@ fn emulation_loop(mut cpu: cpu::CPU) {
     }
 }
 
-fn main() {
-    //    let opt = Opt::from_args();
-    //    println!("{:#?}", opt);
-    ////            let data = fs::read("roms/mooneye/acceptance/timer/tima_write_reloading.gb").unwrap();
-    //            let data = fs::read("roms/mooneye/acceptance/timer/rapid_toggle.gb").unwrap();
-    //        let data = fs::read("roms/mooneye/acceptance/ppu/intr_2_mode0_timing.gb").unwrap();
-    //        let data = fs::read("roms/mooneye/acceptance/oam_dma/sources-GS.gb").unwrap();
-    //            let data = fs::read("roms/mooneye/misc/ppu/vblank_stat_intr-C.gb").unwrap();
-//    let data = fs::read("roms/tetris.gb").unwrap();
-        let data = fs::read("roms/DRMARIO.GB").unwrap();
-//                let data = fs::read("roms/gb-test-roms/interrupt_time/interrupt_time.gb").unwrap();
-    let sdl_context = sdl2::init().unwrap();
+fn main() -> Result<()> {
+    let opt:Opt = Opt::from_args();
+    let data = fs::read(&opt.rom).context(format!("unable to open '{}'", opt.rom.display()))?;
+    let sdl_context= sdl2::init().map_err(|s|anyhow::anyhow!(s))?;
     let mut display = Display::new(&sdl_context);
     let (tx, rx) = mpsc::sync_channel(0);
     let (tx_events, rx_events) = mpsc::channel();
@@ -52,12 +45,10 @@ fn main() {
     let mut redraws = 0u64;
     loop {
         for event in event_pump.poll_iter() {
-            let x = from_sdl2_event(event);
-            if Some(Control::Quit) == x {
-                std::process::exit(0);
-            }
-            if let Some(x) = x {
-                tx_events.send(x).unwrap();
+            match from_sdl2_event(event) {
+                None => {},
+                Some(Control::Quit) => std::process::exit(0),
+                Some(x) => tx_events.send(x)?
             }
         }
         match &rx.try_recv() {
